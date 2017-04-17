4147f5672a27ada4b9282baf0e649cd902d0f04d
gautam <gautam678@gmail.com>
2017-04-17 17:31:35 -0400

    Computing all states and rewards

--- pinball/pinball_deepq.py
+++ pinball/pinball_deepq.py
@@ -43,9 +43,18 @@ model=buildmodel()
 
 # The environment that runs the game
 
+
+BATCH=32
+
+
+def remember(memory,state, action, reward, next_state, done):
+    memory.append((state, action, reward, next_state, done))
+    return memory
+
 env = gym.make('VideoPinball-v0')
 env.reset()
 stacked_image=np.empty(shape=(1,4,84,84))
+memory=[]
 for turns in range(2):
     env.render()
     state=env.reset()
@@ -54,16 +63,21 @@ for turns in range(2):
     state = rescale_intensity(state, out_range=(0, 255))
     state = state.reshape(1, 1, state.shape[0], state.shape[1])
     stacked_image = np.append(state, stacked_image[:, :3, :, :], axis=1)
-    for time_t in range(4,50):
+    for time_t in range(100):
             # turn this on if you want to render
             env.render()
             action=env.action_space.sample()
-  
             # Advance the game to the next frame based on the action.
             next_state, reward, done, _ = env.step(action)
+            old_state=stacked_image
             next_state = scipy.misc.imresize(next_state, [84,84,3])
             next_state = rgb2gray(next_state)
             next_state = resize(next_state,(84,84))
             next_state = rescale_intensity(next_state, out_range=(0, 255))
             next_state = next_state.reshape(1, 1, next_state.shape[0], next_state.shape[1])
             stacked_image = np.append(next_state, stacked_image[:, :3, :, :], axis=1)
+            memory.append((old_state, action, reward, stacked_image, done))
+            val= [x[0].shape for x in memory]
+
+print val
+
